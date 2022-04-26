47be68768e1c4c60a5039808dca2bf9ac8e5f24e
Doug Emery <emeryr@upenn.edu>
2022-04-26 17:31:48 -0400

    Add logging; make methods clearer

--- split_data.rb
+++ split_data.rb
@@ -6,34 +6,33 @@ require 'csv'
 require 'yaml'
 require 'logger'
 
+LOGGER = Logger.new STDOUT
+LOGGER.level = (Logger::DEBUG || ENV['DS_LOGLEVEL'])
 
 OUT_DIR              = File.expand_path '../member-data', __FILE__
 QID_DEFAULT         = 'holding_institution'
 AS_RECORDED_DEFAULT = 'holding_institution_as_recorded'
 DEFAULT_CONFIG      = File.expand_path '../config.yml', __FILE__
 
-options             = {
+options = {
   qid:         QID_DEFAULT,
   as_recorded: AS_RECORDED_DEFAULT,
 }
 
-logger = Logger.new STDOUT
-logger.level = Logger::INFO
-
-def validate_csv csv, options
+##
+# Validate the headers and data
+def validate_csv csv, qid_col:, as_recorded_col:
   errors = []
-  errors << validate_headers(csv, options)
-  errors << validate_data(csv, options)
+  errors << validate_headers(csv, qid_col: qid_col, as_recorded_col: as_recorded_col)
+  errors << validate_data(csv, qid_col: qid_col, as_recorded_col: as_recorded_col)
   errors.compact!
   return if errors.empty?
   raise %Q{Errors encountered:\n#{errors.join "\n"}}
 end
 
-def validate_data csv, options
+def validate_data csv, qid_col:, as_recorded_col:
   missing_qid     = []
   missing_name    = []
-  qid_col         = options[:qid]
-  as_recorded_col = options[:as_recorded]
   row_index = 0
   CSV.foreach csv, headers: true do |row|
     row_index += 1
@@ -45,11 +44,10 @@ def validate_data csv, options
   "Rows missing #{qid_col}: #{missing_qid.size}; and #{as_recorded_col}: #{missing_name.size}"
 end
 
-def validate_headers csv, options
+def validate_headers csv, qid_col:, as_recorded_col:
   headers = CSV.readlines(csv).first
-  # binding.pry
-  missing = %i{qid as_recorded}.flat_map { |k|
-    headers.include?(options[k]) ? [] : "#{k}: '#{options[k]}'"
+  missing = [qid_col, as_recorded_col].flat_map { |header|
+    headers.include?(header) ? [] : header
   }
 
   return if missing.empty?
@@ -57,24 +55,21 @@ def validate_headers csv, options
   "Could not find required column(s) -- #{missing.join ', '}"
 end
 
-def read_institutions csv, options
+def read_institutions csv, qid_col:, as_recorded_col:
   data = {}
-  qid_col = options[:qid]
-  name_col = options[:as_recorded]
   CSV.foreach csv, headers: true do |row|
     qid  = row[qid_col]
     next if qid.to_s.strip.empty?
     next if data[qid_col]
-    as_recorded = row[name_col]
+    as_recorded = row[as_recorded_col]
     next if as_recorded.to_s.strip.empty?
     data[qid] = as_recorded
   end
   data
 end
 
-def check_config config, csv, options
-  csv_institutions = read_institutions csv, options
-    # binding.pry
+def check_config config, csv, qid_col:, as_recorded_col:
+  csv_institutions = read_institutions csv, qid_col: qid_col, as_recorded_col: as_recorded_col
   missing = csv_institutions.keys.map { |qid|
     next if config.any? { |inst| inst[:qid] == qid }
     [qid, csv_institutions[qid]]
@@ -88,8 +83,8 @@ def check_config config, csv, options
       directory: 'REPLACE_WITH_DIR_NAME'
     }
   }.to_yaml.lines[1..-1].join
-  STDERR.puts "CSV has institutions not in the config."
-  STDERR.puts "Complete the following lines and add them to config.yml"
+  LOGGER.error "CSV has institutions not in the config."
+  LOGGER.error "Complete the following lines and add them to config.yml"
   STDERR.puts
   STDERR.puts message
   STDERR.puts
@@ -106,13 +101,13 @@ def validate_config config
   end
   dupes = []
   qids.each do |qid, count|
-    dupes << "QID: #{qid}, couunt: #{count}" if count > 1
+    dupes << "QID: #{qid}, occurrences: #{count}" if count > 1
   end
   dirs.each do |dir, count|
-    dupes << "Directory: #{dir}, couunt: #{count}" if count > 1
+    dupes << "Directory: #{dir}, occurrences: #{count}" if count > 1
   end
   return if dupes.empty?
-  STDERR.puts "Invalid config.yml; the following values are duplicated:"
+  LOGGER.error "Invalid config.yml; the following values are duplicated:"
   STDERR.puts
   STDERR.puts dupes.join "\n"
   STDERR.puts
@@ -137,57 +132,58 @@ ARGV.options do |opts|
     options[:clobber] = clobber
   end
 
+  opts.on '--verbose', 'Be verbose' do |verbose|
+    options[:verbose] = verbose
+  end
+
   opts.parse!
 end
+qid_col = options[:qid]
+name_col = options[:as_recorded]
+
 
 # read the config and fail if it's not present
 abort "Cannot find find config file #{DEFAULT_CONFIG}" unless File.exist? DEFAULT_CONFIG
 config = YAML.load_file DEFAULT_CONFIG
+
 csv = ARGV.shift
-
-
 abort "Please provide a CSV file" unless csv
 abort "Cannot find CSV file: '#{csv}'" unless File.exist? csv
 
+begin
+  validate_config config
+  validate_csv csv, qid_col: qid_col, as_recorded_col: name_col
+  check_config config, csv, qid_col: qid_col, as_recorded_col: name_col
 
-qid_col = options[:qid]
-name_col = options[:as_recorded]
+  header      = CSV.readlines(csv).first
+  out_base    = File.basename csv, '.csv'
+  rows_by_qid = {}
+  CSV.foreach csv, headers: true do |row|
+    qid = row[qid_col]
+    (rows_by_qid[qid] ||= []) << row
+  end
 
-validate_config config
-validate_csv csv, options
-check_config config, csv, options
-
-CSV_NAME_PATTERN = %r{(\d{4}-\d{2}-\d{3})(.*)\.csv}
-csv =~ CSV_NAME_PATTERN
-file_date = $1
-file_detail = $2
-out_base = "#{file_date}#{file_detail}"
-out_base = File.basename(csv, '.csv') if out_base.strip.empty?
-
-header = CSV.readlines(csv).first
-lines_by_qid = {}
-CSV.foreach csv, headers: true do |row|
-  qid = row[qid_col]
-  (lines_by_qid[qid] ||= []) << row
-end
-
-config.each do |inst|
-  next unless lines_by_qid.include? inst[:qid]
-  dir          = inst[:directory]
-  inst_out_dir = File.join OUT_DIR, dir
-  out_file     = File.join inst_out_dir, "#{out_base}-#{dir}.csv"
-  Dir.mkdir inst_out_dir unless Dir.exist? inst_out_dir
-  if File.exist? out_file
-    if options[:clobber]
-      logger.warn { "Overwriting existing file: #{out_file}" }
-    else
-      logger.warn { "File exists; skipping: #{out_file}"}
-      next
+  config.each do |inst|
+    next unless rows_by_qid.include? inst[:qid]
+    dir          = inst[:directory]
+    inst_out_dir = File.join OUT_DIR, dir
+    out_file     = File.join inst_out_dir, "#{out_base}-#{dir}.csv"
+    Dir.mkdir inst_out_dir unless Dir.exist? inst_out_dir
+    if File.exist? out_file
+      if options[:clobber]
+        LOGGER.warn { "Overwriting existing file: #{out_file}" }
+      else
+        LOGGER.warn { "File exists; skipping: #{out_file}" }
+        next
+      end
     end
+    CSV.open out_file, 'wb' do |csv|
+      csv << header
+      rows_by_qid[inst[:qid]].each { |row| csv << row }
+    end
+    LOGGER.info { "Wrote: #{out_file}" }
   end
-  CSV.open out_file, 'wb' do |csv|
-    csv << header
-    lines_by_qid[inst[:qid]].each { |row| csv << row }
-  end
-  logger.info { "Wrote: #{out_file}" }
+rescue StandardError => e
+  LOGGER.fatal e
+  LOGGER.error e.backtrace if options[:verbose]
 end
